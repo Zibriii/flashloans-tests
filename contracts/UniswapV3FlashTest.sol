35a0ffac7d126ad3318a0cc7d1283af20f5a267c
Zibriii <zibriiiii@protonmail.com>
2022-11-25 09:21:41 +0100

    FlashSwap contract

--- contracts/UniswapV3FlashTest.sol
+++ contracts/UniswapV3FlashTest.sol
@@ -7,13 +7,15 @@ pragma solidity >=0.7.0 <0.9.0;
 import "./UniswapV3Flash.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
+
 //contract UniswapV3FlashTest is Test {
 contract UniswapV3FlashTest {
     address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     uint24 constant POOL_FEE = 3000;
 
-    IWETH private weth = IWETH(WETH);
+    //IWETH private weth = IWETH(WETH);
+    IERC20 private weth = IERC20(WETH);
     IERC20 private usdc = IERC20(USDC);
 
     UniswapV3Flash private uni = new UniswapV3Flash(USDC, WETH, POOL_FEE);
@@ -21,6 +23,21 @@ contract UniswapV3FlashTest {
     function setUp() public {}
 
     function testFlash() public {
+        // Approve WETH fee
+        //weth.deposit{value: 1e18}();
+        //weth.approve(address(uni), 1e18);
+
+        uint balBefore = weth.balanceOf(address(this));
+        console.log("balBefore", balBefore);
+        uni.flash(0, 100 * 1e18);
+        uint balAfter = weth.balanceOf(address(this));
+        console.log("balAfter", balAfter);
+
+        uint fee = balBefore - balAfter;
+        console.log("WETH fee", fee);
+    }
+
+/*    function testFlash() public {
         // Approve WETH fee
         weth.deposit{value: 1e18}();
         weth.approve(address(uni), 1e18);
@@ -31,5 +48,5 @@ contract UniswapV3FlashTest {
 
         uint fee = balBefore - balAfter;
         console.log("WETH fee", fee);
-    }
+    }*/
 }
